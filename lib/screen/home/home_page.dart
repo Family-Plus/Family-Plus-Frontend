f6448f18a1fc77b95f7bb634f6b45b83a6b0509c
riyanputraf <riyanputrafirjatullah@gmail.com>
2022-11-26 15:02:39 +0700

    Perbaikan home page

--- lib/screen/home/home_page.dart
+++ lib/screen/home/home_page.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Halaman Home'),
+        title: Text(' Ini Halaman Home'),
       ),
     );
   }
